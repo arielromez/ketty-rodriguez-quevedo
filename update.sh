a0759c59a9e9b11a0fcd244fe2b8f9102d5855c0
arielromez <arielrodmez@gmail.com>
2025-11-24 23:14:25 -0500

    Update website

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git add -A
+git commit -m "Update website"
+git push
+echo "✓ Changes pushed to GitHub. Your site will update in 1-2 minutes at:"
+echo "https://arielromez.github.io/ketty-rodriguez-quevedo/"
